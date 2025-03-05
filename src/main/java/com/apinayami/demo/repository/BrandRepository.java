a1a1a41d9f12374d98d964a11e4348d22228b27b
QuangDang <hadangquang1408@gmail.com>
2025-03-05 22:45:21 +0700

    Fix full. Test data and fix model follow to class diagram

--- src/main/java/com/apinayami/demo/repository/BrandRepository.java
+++
@@ -1,7 +0,0 @@
-package com.apinayami.demo.repository;
-
-import com.apinayami.demo.model.BrandModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IBrandRepository extends JpaRepository<BrandModel,Long> {
-}
--- src/main/java/com/apinayami/demo/repository/IBrandRepository.java
+++ src/main/java/com/apinayami/demo/repository/IBrandRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BrandRepository extends JpaRepository<BrandModel, Long> {
+public interface IBrandRepository extends JpaRepository<BrandModel, Long> {
 }
