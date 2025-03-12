f15710e269fecf8e1c2a684e2974e4121566e454
BaoStar0990 <slamandertranbao2712@gmail.com>
2025-03-12 12:44:28 +0700

    Category CRUD

---
+++ src/main/java/com/apinayami/demo/mapper/CategoryMapper.java
@@ -0,0 +1,14 @@
+package com.apinayami.demo.mapper;
+
+import com.apinayami.demo.dto.request.CategoryDTO;
+import com.apinayami.demo.model.CategoryModel;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CategoryMapper {
+    CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
+
+    CategoryModel toCategoryModel(CategoryDTO dto);
+    CategoryDTO toCategoryDTO(CategoryModel model);
+}
--- src/main/java/com/apinayami/demo/service/ICategoryService.java
+++ src/main/java/com/apinayami/demo/service/ICategoryService.java
@@ -1,6 +1,13 @@
 package com.apinayami.demo.service;
 
+import com.apinayami.demo.dto.request.CategoryDTO;
 import com.apinayami.demo.model.CategoryModel;
 
+import java.util.List;
+
 public interface ICategoryService extends IBaseCRUD<CategoryModel> {
+
+    List<CategoryDTO> getAll();
+    CategoryModel findById(long id);
+
 }
