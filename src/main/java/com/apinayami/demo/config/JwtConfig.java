7cb39a9548174d86f90841abee613e2e0848fdbb
TPhong <phongga088@gmail.com>
2025-04-22 15:57:27 +0700

    fix send 2 request

--- src/main/java/com/apinayami/demo/config/JwtConfig.java
+++ src/main/java/com/apinayami/demo/config/JwtConfig.java
@@ -3,53 +3,54 @@ package com.apinayami.demo.config;
 import com.apinayami.demo.util.SecurityUtil;
 import com.nimbusds.jose.jwk.source.ImmutableSecret;
 import com.nimbusds.jose.util.Base64;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.*;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
-import org.springframework.security.web.FilterChainProxy;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
 /*
-* Lớp này dùng để cấu hình jwt encoder và decoder
-* */
+ * Lớp này dùng để cấu hình jwt encoder và decoder
+ * */
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
+@Slf4j
 public class JwtConfig {
     @Value("${JWT_SECRET_KEY}")
     private String jwtSecretKey;
-    private SecretKey getSecretKey(){
+
+    private SecretKey getSecretKey() {
         byte[] keyBytes = Base64.from(jwtSecretKey).decode(); //giai ma
-        return new SecretKeySpec(keyBytes, 0,keyBytes.length,SecurityUtil.JWT_ALGORITHM.getName());
+        return new SecretKeySpec(keyBytes, 0, keyBytes.length, SecurityUtil.JWT_ALGORITHM.getName());
     }
+
     @Bean
-    public JwtEncoder jwtEncoder(){
+    public JwtEncoder jwtEncoder() {
         return new NimbusJwtEncoder(new ImmutableSecret<>(getSecretKey()));
     }
+
     @Bean
-    public JwtDecoder jwtDecoder(){
+    public JwtDecoder jwtDecoder() {
         NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.
                 withSecretKey(getSecretKey()).
                 macAlgorithm(SecurityUtil.JWT_ALGORITHM).
                 build();
-        return token ->{
-           try{
-               Jwt jwt = jwtDecoder.decode(token);
-               return jwt;
-           }catch(Exception e){
-               throw new RuntimeException(e);
-           }
+        return token -> {
+            try {
+                Jwt jwt = jwtDecoder.decode(token);
+                return jwt;
+            } catch (Exception e) {
+                log.error(e.getMessage());
+                return null;
+            }
         };
     }
-    public Jwt decodeToken(String token){
+
+    public Jwt decodeToken(String token) {
         NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.
                 withSecretKey(getSecretKey()).
                 macAlgorithm(SecurityUtil.JWT_ALGORITHM).
--- src/main/java/com/apinayami/demo/config/WebSecurityConfig.java
+++ src/main/java/com/apinayami/demo/config/WebSecurityConfig.java
@@ -10,14 +10,9 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
 import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 
 @Configuration
@@ -33,6 +28,7 @@ public class WebSecurityConfig {
             "/api/login",
             "/api/signup",
             "/api/users",
+            "/api/users/check",
             "/api/users/create",
             "/api/reset-password",
             "/api/check-email-exist",
@@ -71,6 +67,7 @@ public class WebSecurityConfig {
 
 
     private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
